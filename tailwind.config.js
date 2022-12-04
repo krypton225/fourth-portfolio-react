b413d305778d33f4fe9368162426f9421c82c484
Krypton <k.mohamed.11.98@gmail.com>
2022-12-05 00:20:56 +0200

    ADD custom colors @ tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "rich-black": "#0A0908",
+                'white': '#ffffff',
+                "dark-green": "#22333B",
+                "light-beige": "#A9927D",
+                "dark-beige": "#5E503F",
+            },
+        },
+    },
+    plugins: [],
+}
