21dfbdbba7508a26558516b680014b56b02238b3
Krypton <k.mohamed.11.98@gmail.com>
2022-12-05 16:52:27 +0200

    ADD url icons & titles @ social navbar

--- src/data/socialMedia.js
+++ src/data/socialMedia.js
@@ -4,11 +4,11 @@ import { BsTwitter, BsInstagram } from "react-icons/bs";
 import insert from "./generateIDs";
 
 const socialMediaIcons = [
-    { icon: <FaFacebookF /> },
-    { icon: <BsTwitter /> },
-    { icon: <BsInstagram /> },
-    { icon: <FaWhatsapp /> },
-    { icon: <FaTelegramPlane /> },
+    { icon: <FaFacebookF />, urlIcon: "https://ar-ar.facebook.com/", titleIcon: "Facebook" },
+    { icon: <BsTwitter />, urlIcon: "https://twitter.com/", titleIcon: "Twitter" },
+    { icon: <BsInstagram />, urlIcon: "https://www.instagram.com/", titleIcon: "Instagram" },
+    { icon: <FaWhatsapp />, urlIcon: "https://web.whatsapp.com/", titleIcon: "Whatsapp" },
+    { icon: <FaTelegramPlane />, urlIcon: "https://web.telegram.org/", titleIcon: "Telegram" },
 ];
 
 (() => insert(socialMediaIcons))();
