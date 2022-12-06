11384fe6fea452e3fe57d861d1b04e7b7a103a29
Krypton <k.mohamed.11.98@gmail.com>
2022-12-06 16:55:18 +0200

    ADD responsive for images @ Branding comp

--- src/components/Branding.jsx
+++ src/components/Branding.jsx
@@ -4,8 +4,8 @@ const Branding = () => (
     <section className="relative w-screen mt-5 py-1 px-5 text-center mx-auto flex justify-center items-center bg-[#1a1a1a]">
         {
             AllBrandingImages.map(({ id, imageSrc, imageAlt }) => (
-                <div key={id} className="w-1/2 mx-auto flex justify-center items-center">
-                    <img src={imageSrc} alt={imageAlt} className="p-4 cursor-pointer" />
+                <div key={id} className="w-3/4 lg:w-1/2 mx-auto flex justify-center items-center">
+                    <img src={imageSrc} alt={imageAlt} className="p-1 lg:p-4 cursor-pointer" />
                 </div>
             ))
         }
