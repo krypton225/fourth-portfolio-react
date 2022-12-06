8797c68cc0cc1404822a94a8bbe952dd35127afe
Krypton <k.mohamed.11.98@gmail.com>
2022-12-06 19:23:28 +0200

    ADD selection colors for text globally

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Navbar, Landing, Branding, About } from "./components/index";
 
 function App() {
   return (
-    <div className="app h-auto relative overflow-x-hidden">
+    <div className="app h-auto relative overflow-x-hidden selection:text-rich-black selection:bg-light-beige">
       <Navbar />
       <Landing />
       <Branding />
