d38ea5f42340e300a71240cce7b8910027a49755
Krypton <k.mohamed.11.98@gmail.com>
2022-12-06 16:50:31 +0200

    REM logs from Branding comp

--- src/components/Branding.jsx
+++ src/components/Branding.jsx
@@ -3,9 +3,6 @@ import AllBrandingImages from "../data/branding-imgs";
 const Branding = () => (
     <section className="relative w-screen mt-5 py-1 px-5 text-center mx-auto flex justify-center items-center bg-[#1a1a1a]">
         {
-            console.log(AllBrandingImages)
-        }
-        {
             AllBrandingImages.map(({ id, imageSrc, imageAlt }) => (
                 <div key={id} className="w-1/2 mx-auto flex justify-center items-center">
                     <img src={imageSrc} alt={imageAlt} className="p-4 cursor-pointer" />
