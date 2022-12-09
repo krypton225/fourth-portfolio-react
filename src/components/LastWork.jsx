41d6401146b744225820dc3886c813f1b7657a05
Krypton <k.mohamed.11.98@gmail.com>
2022-12-09 21:14:25 +0200

    ADD animations @ LastWork component

--- src/components/LastWork.jsx
+++ src/components/LastWork.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import { Heading } from "./index";
 
 import AllProjectsImages from "../data/projects-imgs.js";
 
 const LastWork = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
         <section id="portfolio">
             <div className="container py-36 px-8 text-center">
@@ -15,8 +24,8 @@ const LastWork = () => {
 
                 <div className="w-full mt-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 md:gap-8">
                     {
-                        AllProjectsImages.map(({ id, imageSrc, imageAlt, projectCategory }) => (
-                            <div key={id}>
+                        AllProjectsImages.map(({ id, imageSrc, imageAlt, projectCategory, dataAOS, dataAOSDelay, dataAOSDuration }) => (
+                            <div key={id} data-aos={dataAOS} data-aos-easing="linear" data-aos-duration={dataAOSDuration} data-aos-delay={dataAOSDelay}>
                                 <div className="w-full rounded-3xl overflow-hidden group transition-all duration-[0.9s]">
                                     <img src={imageSrc} alt={imageAlt} draggable="false" className="w-full rounded-3xl transition-all duration-[0.9s] group-hover:scale-110 group-hover:-rotate-[5deg]" />
                                 </div>
--- src/data/projects-imgs.js
+++ src/data/projects-imgs.js
@@ -3,12 +3,44 @@ import insert from "./generateIDs";
 import { ImageProject1, ImageProject2, ImageProject3, ImageProject4, ImageProject5, ImageProject6 } from "./imgs-imports"
 
 const projectsImages = [
-    { imageSrc: ImageProject1, imageAlt: "Laptop, Phone and Camera", projectCategory: "Laptop, Phone and Camera" },
-    { imageSrc: ImageProject2, imageAlt: "Phone", projectCategory: "Phone" },
-    { imageSrc: ImageProject3, imageAlt: "Laptop", projectCategory: "Laptop" },
-    { imageSrc: ImageProject4, imageAlt: "Phone with Laptop", projectCategory: "Phone with Laptop" },
-    { imageSrc: ImageProject5, imageAlt: "Headphone", projectCategory: "Headphone" },
-    { imageSrc: ImageProject6, imageAlt: "Watch", projectCategory: "Watch" },
+    {
+        imageSrc: ImageProject1,
+        imageAlt: "Laptop, Phone and Camera",
+        projectCategory: "Laptop, Phone and Camera",
+        dataAOS: "zoom-in",
+        dataAOSDuration: "800",
+        dataAOSDelay: "150"
+    },
+    {
+        imageSrc: ImageProject2, imageAlt: "Phone", projectCategory: "Phone",
+        dataAOS: "zoom-in",
+        dataAOSDuration: "800",
+        dataAOSDelay: "150"
+    },
+    {
+        imageSrc: ImageProject3, imageAlt: "Laptop", projectCategory: "Laptop",
+        dataAOS: "zoom-in",
+        dataAOSDuration: "800",
+        dataAOSDelay: "150"
+    },
+    {
+        imageSrc: ImageProject4, imageAlt: "Phone with Laptop", projectCategory: "Phone with Laptop",
+        dataAOS: "zoom-in",
+        dataAOSDuration: "800",
+        dataAOSDelay: "150"
+    },
+    {
+        imageSrc: ImageProject5, imageAlt: "Headphone", projectCategory: "Headphone",
+        dataAOS: "zoom-in",
+        dataAOSDuration: "800",
+        dataAOSDelay: "150"
+    },
+    {
+        imageSrc: ImageProject6, imageAlt: "Watch", projectCategory: "Watch",
+        dataAOS: "zoom-in",
+        dataAOSDuration: "800",
+        dataAOSDelay: "150"
+    },
 ];
 
 (() => insert(projectsImages))();
