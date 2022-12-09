a95528290da6802062d914a006b450801463b4c9
Krypton <k.mohamed.11.98@gmail.com>
2022-12-09 21:06:36 +0200

    ADD animations @ Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,30 @@
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 import AllSkillsImages from "../data/skills-imgs";
 
-const Skills = () => (
-    <section className="w-screen relative py-5 px-5 text-center mx-auto flex justify-center items-center bg-[#1a1a1a]">
-        {
-            AllSkillsImages.map(({ id, imageSrc, imageAlt }) => (
-                <div key={id} className="w-1/6 mx-auto flex flex-wrap justify-center items-center">
-                    <LazyLoadImage effect='blur' threshold={150} src={imageSrc} alt={imageAlt} className="p-1 lg:p-4 cursor-pointer" draggable="false" />
-                </div>
-            ))
-        }
-    </section>
-)
+const Skills = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+    return (
+        <section className="w-screen relative py-5 px-5 text-center mx-auto flex justify-center items-center bg-[#1a1a1a] overflow-hidden">
+            {
+                AllSkillsImages.map(({ id, imageSrc, imageAlt, dataAOS, dataAOSDuration, dataAOSDelay }) => (
+                    <div key={id} className="w-1/6 mx-auto flex flex-wrap justify-center items-center"
+                        data-aos={dataAOS} data-aos-easing="linear" data-aos-duration={dataAOSDuration} data-aos-delay={dataAOSDelay}>
+                        <LazyLoadImage effect='blur' threshold={150} src={imageSrc} alt={imageAlt} className="p-1 lg:p-4 cursor-pointer" draggable="false" />
+                    </div>
+                ))
+            }
+        </section>
+    )
+}
 
 export default Skills;
--- src/data/skills-imgs.js
+++ src/data/skills-imgs.js
@@ -12,14 +12,14 @@ import {
 } from "./imgs-imports";
 
 const skillsImages = [
-    { imageSrc: ImageSkillsHTML5, imageAlt: "HTML5" },
-    { imageSrc: ImageSkillsCSS3, imageAlt: "CSS3" },
-    { imageSrc: ImageSkillsJavaScript, imageAlt: "JavaScript" },
-    { imageSrc: ImageSkillsGit, imageAlt: "Git" },
-    { imageSrc: ImageSkillsFigma, imageAlt: "Figma" },
-    { imageSrc: ImageSkillsReactJS, imageAlt: "React JS" },
-    { imageSrc: ImageSkillsNodeJS, imageAlt: "Node JS" },
-    { imageSrc: ImageSkillsNextJS, imageAlt: "NextJS" },
+    { imageSrc: ImageSkillsHTML5, imageAlt: "HTML5", dataAOS: "zoom-out", dataAOSDuration: "500", dataAOSDelay: "150" },
+    { imageSrc: ImageSkillsCSS3, imageAlt: "CSS3", dataAOS: "zoom-out", dataAOSDuration: "500", dataAOSDelay: "300" },
+    { imageSrc: ImageSkillsJavaScript, imageAlt: "JavaScript", dataAOS: "zoom-out", dataAOSDuration: "500", dataAOSDelay: "450" },
+    { imageSrc: ImageSkillsGit, imageAlt: "Git", dataAOS: "zoom-out", dataAOSDuration: "500", dataAOSDelay: "600" },
+    { imageSrc: ImageSkillsFigma, imageAlt: "Figma", dataAOS: "zoom-out", dataAOSDuration: "500", dataAOSDelay: "750" },
+    { imageSrc: ImageSkillsReactJS, imageAlt: "React JS", dataAOS: "zoom-out", dataAOSDuration: "500", dataAOSDelay: "900" },
+    { imageSrc: ImageSkillsNodeJS, imageAlt: "Node JS", dataAOS: "zoom-out", dataAOSDuration: "500", dataAOSDelay: "1050" },
+    { imageSrc: ImageSkillsNextJS, imageAlt: "NextJS", dataAOS: "zoom-out", dataAOSDuration: "500", dataAOSDelay: "1200" },
 ];
 
 (() => insert(skillsImages))();
