85188537569382a0811a82c53635cc2f9bcf9870
Krypton <k.mohamed.11.98@gmail.com>
2022-12-09 21:30:12 +0200

    ADD animations @ Services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,35 +1,46 @@
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import { Heading } from "./index";
 
 import AllServicesInfo from "../data/services-info.js";
 
-const Services = () => (
-    <section id="services" className="py-40 px-2 md:px-8 bg-[#1a1a1a]">
-        <div className="container text-center">
-            <Heading textSentence={"what I do for clients"} />
+const Services = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
 
-            <p className="w-[90%] lg:w-[80%] mt-6 mx-auto text-white text-sm lg:text-lg leading-6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt laudantium officia fuga ipsam quasi corrupti in numquam dolor libero explicabo, inventore aperiam, commodi et!
-            </p>
+    return (
+        <section id="services" className="py-40 px-2 md:px-8 bg-[#1a1a1a]">
+            <div className="container text-center">
+                <Heading textSentence={"what I do for clients"} />
 
-            <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-4 text-start">
-                {
-                    AllServicesInfo.map(({ id, serviceIcon, serviceAltText, serviceTitle, serviceDescription }) => (
-                        <div key={id} className="rounded-xl py-6 px-4 bg-[#222] flex flex-col justify-between items-center cursor-pointer
-                            transition duration-[1.2s] hover:bg-[#444]">
-                            <div className="w-full mx-0 text-start">
-                                <img src={serviceIcon} alt={serviceAltText} />
+                <p className="w-[90%] lg:w-[80%] mt-6 mx-auto text-white text-sm lg:text-lg leading-6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt laudantium officia fuga ipsam quasi corrupti in numquam dolor libero explicabo, inventore aperiam, commodi et!
+                </p>
+
+                <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-4 text-start">
+                    {
+                        AllServicesInfo.map(({ id, serviceIcon, serviceAltText, serviceTitle, serviceDescription, dataAOS, delay, dataAOSDuration }) => (
+                            <div key={id} className="rounded-xl py-6 px-4 bg-[#222] flex flex-col justify-between items-center cursor-pointer
+                                transition-all duration-[1.2s] hover:bg-[#444]" data-aos={dataAOS} data-aos-easing="linear" data-aos-duration={dataAOSDuration} data-aos-delay={delay}>
+                                <div className="w-full mx-0 text-start">
+                                    <img src={serviceIcon} alt={serviceAltText} />
+                                </div>
+
+                                <div className="w-full mt-12 text-white">
+                                    <h4 className="text-2xl capitalize mb-4">{serviceTitle}</h4>
+                                    <p>{serviceDescription}</p>
+                                </div>
                             </div>
-
-                            <div className="w-full mt-12 text-white">
-                                <h4 className="text-2xl capitalize mb-4">{serviceTitle}</h4>
-                                <p>{serviceDescription}</p>
-                            </div>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+                </div>
             </div>
-        </div>
-    </section>
-);
+        </section>
+    )
+};
 
 export default Services;
--- src/data/services-info.js
+++ src/data/services-info.js
@@ -7,25 +7,37 @@ const servicesInfo = [
         serviceIcon: ImageServicesBranding,
         serviceAltText: "branding icon",
         serviceTitle: "branding",
-        serviceDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt laudantium officia fuga ipsam quasi."
+        serviceDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt laudantium officia fuga ipsam quasi.",
+        dataAOS: "zoom-in",
+        dataAOSDuration: "500",
+        delay: "50",
     },
     {
         serviceIcon: ImageServicesDesign,
         serviceAltText: "design  icon",
         serviceTitle: "design",
-        serviceDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt laudantium officia fuga ipsam quasi."
+        serviceDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt laudantium officia fuga ipsam quasi.",
+        dataAOS: "zoom-in",
+        dataAOSDuration: "500",
+        delay: "200",
     },
     {
         serviceIcon: ImageServicesDevelopment,
         serviceAltText: "development icon",
         serviceTitle: "development",
-        serviceDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt laudantium officia fuga ipsam quasi."
+        serviceDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt laudantium officia fuga ipsam quasi.",
+        dataAOS: "zoom-in",
+        dataAOSDuration: "500",
+        delay: "350",
     },
     {
         serviceIcon: ImageServicesSEO,
         serviceAltText: "SEO icon",
         serviceTitle: "SEO",
-        serviceDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt laudantium officia fuga ipsam quasi."
+        serviceDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deserunt laudantium officia fuga ipsam quasi.",
+        dataAOS: "zoom-in",
+        dataAOSDuration: "500",
+        delay: "500",
     },
 ];
 
